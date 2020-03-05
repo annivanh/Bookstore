27009a80eb7773d12175ed1f507c53a12f5fcd9c
annivanh <58463622+annivanh@users.noreply.github.com>
2020-03-05 18:20:25 +0200

    Lisätty REST palveluita BookControlleriin

--- Bookstore/src/main/java/swd20/Bookstore/webcontrol/BookController.java
+++ Bookstore/src/main/java/swd20/Bookstore/webcontrol/BookController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import swd20.Bookstore.domain.Book;
 import swd20.Bookstore.domain.BookRepository;
 import swd20.Bookstore.domain.CategoryRepository;
@@ -18,20 +20,29 @@ import swd20.Bookstore.domain.CategoryRepository;
 public class BookController {
 
 	@Autowired
-	BookRepository BookRepository;
+	BookRepository bookRepository;
 	@Autowired 
 	CategoryRepository categoryRepository;
 	
-	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	@RequestMapping(value = "/booklist")
 	public String showBooks(Model model) {
-
-		List<Book> books = (List<Book>) BookRepository.findAll();
-		model.addAttribute("book", new Book());
-		model.addAttribute("books", books);
+		model.addAttribute("books", bookRepository.findAll());
 		return "booklist";
 	}
+	
+	//REST show books
+	@RequestMapping(value="/books", method=RequestMethod.GET)
+	public @ResponseBody List<Book> booklistRest(){
+		return (List<Book>) bookRepository.findAll();
+	}
+	
+	//REST show book by id
+	@RequestMapping(value="/books/{id}", method=RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId){ 
+	return bookRepository.findById(bookId);
+	}
 
-	@RequestMapping(value = "/newbook", method = RequestMethod.GET)
+	@RequestMapping(value = "/newbook")
 	public String addNewBook(Model model) {
 		model.addAttribute("book", new Book());
 		model.addAttribute("categories", categoryRepository.findAll());
@@ -40,26 +51,27 @@ public class BookController {
 
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String saveBook(@ModelAttribute Book book) {
-		BookRepository.save(book);
+		bookRepository.save(book);
 		return "redirect:booklist";
 	}
 
 	@RequestMapping(value = "/deletebook/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId) {
-		BookRepository.deleteById(bookId);
+		bookRepository.deleteById(bookId);
 		return "redirect:../booklist";
 	}
 
 	@GetMapping(value = "/editbook/{id}")
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
-		Optional<Book> book = BookRepository.findById(bookId);
+		Optional<Book> book = bookRepository.findById(bookId);
 		model.addAttribute("book", book);
+		model.addAttribute("categories", categoryRepository.findAll());
 		return "editbook";
 	}
 
 	@RequestMapping(value = "/editbook/{id}", method = RequestMethod.POST)
 	public String save(@ModelAttribute Book book) {
-		BookRepository.save(book);
+		bookRepository.save(book);
 		return "redirect:/booklist";
 	}
 
--- Bookstore/src/test/java/swd20/Bookstore/BookstoreApplicationTests.java
+++ Bookstore/src/test/java/swd20/Bookstore/BookstoreApplicationTests.java
@@ -1,6 +1,6 @@
 package swd20.Bookstore;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
